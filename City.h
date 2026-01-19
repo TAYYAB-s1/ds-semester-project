6ca6b3291c6567e360dab931010f76d715353e31
zahrahsan2005 <zahrahsanbusiness@gmail.com>
2026-01-20 00:15:13 +0500

    Update City.h

--- City.h
+++ City.h
@@ -1,1 +1,29 @@
 
+#ifndef CITY_H
+#define CITY_H
+
+// Custom edge structure (NO STL graph)
+struct Edge {
+    int to;
+    int weight;
+    Edge* next;
+};
+
+class City {
+private:
+    int numNodes;
+    Edge** adjacencyList;   // array of linked lists
+    int* zone;              // zone of each node
+
+public:
+    City(int nodes);
+    ~City();
+
+    void addRoad(int from, int to, int distance);
+    void setZone(int node, int zoneId);
+    int getZone(int node) const;
+
+    int shortestPath(int src, int dest);
+};
+
+#endif
