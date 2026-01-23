d893f9201b9f7b554d42778d924eef40b1e58e10
zahrahsan2005 <zahrahsanbusiness@gmail.com>
2026-01-23 17:29:36 +0500

    added edge data ,resize, addndoe func to City.h

--- City.h
+++ City.h
@@ -23,3 +23,39 @@ City::~City() {
     delete[] nodes;
 }
 
+void City::resize() {
+    int newCapacity = capacity * 2;
+    Node* newNodes = new Node[newCapacity];
+    
+    for (int i = 0; i < nodeCount; i++) {
+        newNodes[i] = nodes[i];
+    }
+    
+    for (int i = nodeCount; i < newCapacity; i++) {
+        newNodes[i].edges = nullptr;
+    }
+    
+    delete[] nodes;
+    nodes = newNodes;
+    capacity = newCapacity;
+}
+
+void City::addNode(int id, string name, int zone) {
+    if (nodeCount >= capacity) {
+        resize();
+    }
+    nodes[nodeCount].id = id;
+    nodes[nodeCount].name = name;
+    nodes[nodeCount].zone = zone;
+    nodes[nodeCount].edges = nullptr;
+    nodeCount++;
+}
+
+void City::addEdge(int from, int to, int distance) {
+    Edge* newEdge = new Edge;
+    newEdge->destination = to;
+    newEdge->distance = distance;
+    newEdge->next = nodes[from].edges;
+    nodes[from].edges = newEdge;
+}
+
