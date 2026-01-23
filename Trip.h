5f86175ee48fa78bb8809ceabbc19b5a22607363
zahrahsan2005 <zahrahsanbusiness@gmail.com>
2026-01-23 17:55:54 +0500

    Update Trip.h

--- Trip.h
+++ Trip.h
@@ -1,1 +1,46 @@
+#ifndef TRIP_H
+#define TRIP_H
 
+#include <string>
+
+using namespace std;
+
+enum TripState {
+    REQUESTED,
+    ASSIGNED,
+    ONGOING,
+    COMPLETED,
+    CANCELLED
+};
+
+class Trip {
+private:
+    int id;
+    int riderId;
+    int driverId;
+    int pickupLocation;
+    int dropoffLocation;
+    int distance;
+    TripState state;
+    
+public:
+    Trip();
+    Trip(int tripId, int rider, int pickup, int dropoff, int dist);
+    
+    int getId();
+    int getRiderId();
+    int getDriverId();
+    int getPickupLocation();
+    int getDropoffLocation();
+    int getDistance();
+    TripState getState();
+    
+    void assignDriver(int driver);
+    void startTrip();
+    void completeTrip();
+    void cancelTrip();
+    
+    string getStateString();
+};
+
+#endif
