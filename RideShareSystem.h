85cadb6e1267dcbe70f4a757a8e8fe1c327318ab
zahrahsan2005 <zahrahsanbusiness@gmail.com>
2026-01-23 17:56:55 +0500

    added RideShareSystem.h

--- RideShareSystem.h
+++ RideShareSystem.h
@@ -1,1 +1,46 @@
+#ifndef RIDESHARESYSTEM_H
+#define RIDESHARESYSTEM_H
 
+#include "City.h"
+#include "Driver.h"
+#include "Rider.h"
+#include "Trip.h"
+#include "DispatchEngine.h"
+#include "RollbackManager.h"
+
+using namespace std;
+
+class RideShareSystem {
+private:
+    City* city;
+    DispatchEngine* dispatch;
+    RollbackManager* rollback;
+    Trip* trips;
+    int tripCount;
+    int tripCapacity;
+    int nextTripId;
+    
+    void resizeTrips();
+    Trip* findTrip(int tripId);
+    
+public:
+    RideShareSystem();
+    ~RideShareSystem();
+    
+    void initializeCity();
+    void addDriver(Driver driver);
+    int requestTrip(Rider rider);
+    bool assignDriver(int tripId);
+    bool startTrip(int tripId);
+    bool completeTrip(int tripId);
+    bool cancelTrip(int tripId);
+    void rollbackOperations(int k);
+    
+    void displayTripHistory();
+    void displayAnalytics();
+    
+    int getTripCount();
+    Trip* getTrip(int index);
+};
+
+#endif
