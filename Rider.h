87a8923deece68b9e867cb667fb88c19488fea0c
zahrahsan2005 <zahrahsanbusiness@gmail.com>
2026-01-23 17:53:47 +0500

    Updated Rider.h

--- Rider.h
+++ Rider.h
@@ -1,1 +1,26 @@
 
+#ifndef RIDER_H
+#define RIDER_H
+
+#include <string>
+
+using namespace std;
+
+class Rider {
+private:
+    int id;
+    string name;
+    int pickupLocation;
+    int dropoffLocation;
+    
+public:
+    Rider();
+    Rider(int riderId, string riderName, int pickup, int dropoff);
+    
+    int getId();
+    string getName();
+    int getPickupLocation();
+    int getDropoffLocation();
+};
+
+#endif
