e675384cb9c4a293a7a74c6929b8097e08e1d8e9
zahrahsan2005 <zahrahsanbusiness@gmail.com>
2026-01-23 17:51:45 +0500

    updated City.h

--- City.h
+++ City.h
@@ -1,126 +1,43 @@
-#include "../header/City.h"
+#ifndef CITY_H
+#define CITY_H
+
+#include <iostream>
+#include <string>
 
 using namespace std;
 
-City::City(int initialCapacity) {
-    capacity = initialCapacity;
-    nodeCount = 0;
-    nodes = new Node[capacity];
-    for (int i = 0; i < capacity; i++) {
-        nodes[i].edges = nullptr;
-    }
-}
+struct Edge {
+    int destination;
+    int distance;
+    Edge* next;
+};
 
-City::~City() {
-    for (int i = 0; i < nodeCount; i++) {
-        Edge* current = nodes[i].edges;
-        while (current != nullptr) {
-            Edge* temp = current;
-            current = current->next;
-            delete temp;
-        }
-    }
-    delete[] nodes;
-}
+struct Node {
+    int id;
+    string name;
+    int zone;
+    Edge* edges;
+};
 
-void City::resize() {
-    int newCapacity = capacity * 2;
-    Node* newNodes = new Node[newCapacity];
+class City {
+private:
+    Node* nodes;
+    int nodeCount;
+    int capacity;
     
-    for (int i = 0; i < nodeCount; i++) {
-        newNodes[i] = nodes[i];
-    }
+    void resize();
+    int* dijkstra(int start);
     
-    for (int i = nodeCount; i < newCapacity; i++) {
-        newNodes[i].edges = nullptr;
-    }
+public:
+    City(int initialCapacity = 50);
+    ~City();
     
-    delete[] nodes;
-    nodes = newNodes;
-    capacity = newCapacity;
-}
+    void addNode(int id, string name, int zone);
+    void addEdge(int from, int to, int distance);
+    int findShortestPath(int from, int to);
+    int getNodeZone(int nodeId);
+    int getNodeCount();
+    Node* getNode(int index);
+};
 
-void City::addNode(int id, string name, int zone) {
-    if (nodeCount >= capacity) {
-        resize();
-    }
-    nodes[nodeCount].id = id;
-    nodes[nodeCount].name = name;
-    nodes[nodeCount].zone = zone;
-    nodes[nodeCount].edges = nullptr;
-    nodeCount++;
-}
-
-void City::addEdge(int from, int to, int distance) {
-    Edge* newEdge = new Edge;
-    newEdge->destination = to;
-    newEdge->distance = distance;
-    newEdge->next = nodes[from].edges;
-    nodes[from].edges = newEdge;
-}
-int* City::dijkstra(int start) {
-    int* distances = new int[nodeCount];
-    bool* visited = new bool[nodeCount];
-    
-    for (int i = 0; i < nodeCount; i++) {
-        distances[i] = 2147483647;
-        visited[i] = false;
-    }
-    
-    distances[start] = 0;
-    
-    for (int count = 0; count < nodeCount - 1; count++) {
-        int minDist = 2147483647;
-        int minIndex = -1;
-        
-        for (int v = 0; v < nodeCount; v++) {
-            if (!visited[v] && distances[v] < minDist) {
-                minDist = distances[v];
-                minIndex = v;
-            }
-        }
-        
-        if (minIndex == -1) break;
-        
-        visited[minIndex] = true;
-        
-        Edge* edge = nodes[minIndex].edges;
-        while (edge != nullptr) {
-            int v = edge->destination;
-            if (!visited[v] && distances[minIndex] != 2147483647 &&
-                distances[minIndex] + edge->distance < distances[v]) {
-                distances[v] = distances[minIndex] + edge->distance;
-            }
-            edge = edge->next;
-        }
-    }
-    
-    delete[] visited;
-    return distances;
-}
-
-int City::findShortestPath(int from, int to) {
-    int* distances = dijkstra(from);
-    int result = distances[to];
-    delete[] distances;
-    return result;
-}
-
-int City::getNodeZone(int nodeId) {
-    if (nodeId >= 0 && nodeId < nodeCount) {
-        return nodes[nodeId].zone;
-    }
-    return -1;
-}
-
-int City::getNodeCount() {
-    return nodeCount;
-}
-
-Node* City::getNode(int index) {
-    if (index >= 0 && index < nodeCount) {
-        return &nodes[index];
-    }
-    return nullptr;
-}
-
+#endif
